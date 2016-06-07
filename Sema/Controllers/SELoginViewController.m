be3b7751f9dc5dd4a5d2b8c8338b1f13f2016126
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-06-07 23:57:06 +0200

    Working login

--- Sema/Controllers/SELoginViewController.m
+++ Sema/Controllers/SELoginViewController.m
@@ -14,11 +14,16 @@
 
 @interface SELoginViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *loginTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
 @property (strong, nonatomic) SELoginViewModel *viewModel;
 
 @end
 
 @implementation SELoginViewController
+
+static NSString *const SEAfterLoginSegueID = @"SEAfterLoginSegueID";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -29,11 +34,38 @@
     // Do any additional setup after loading the view.
 }
 
-
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
     [self.view endEditing:YES];
+}
+
+#pragma mark -
+#pragma mark - Actions
+
+- (IBAction)loginAction:(id)sender {
+    
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    
+    [self.view endEditing:YES];
+    
+    __weak typeof (self) wSelf = self;
+    [_viewModel loginWithCompletion:^(BOOL success, UIAlertController *alert) {
+        [MBProgressHUD hideHUDForView:wSelf.view animated:YES];
+        
+        if (!success && alert) {
+            [wSelf presentViewController:alert animated:YES completion:nil];
+            
+            return ;
+        }
+        
+        [wSelf performSegueWithIdentifier:SEAfterLoginSegueID sender:nil];
+    }];
+}
+
+- (IBAction)textFieldTextHasChangedAction:(id)sender {
+    self.viewModel.password = self.passwordTextField.text;
+    self.viewModel.login = self.loginTextField.text;
 }
 
 - (IBAction)tapOnViewAction:(id)sender {
